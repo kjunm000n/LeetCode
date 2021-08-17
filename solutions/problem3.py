aa537a3f37e76a8c302eab84c8dbda64a0337e19
kjunm000n <kjunm000n@github.com>
2021-08-17 12:48:17 +0900

    Add) Problem3

---
+++ solutions/problem3.py
@@ -0,0 +1,70 @@
+# [3] Longest Substring Without Repeating Characters
+
+import random, string
+
+from solutions.interface import ProblemInterface
+
+
+class Problem3(ProblemInterface):
+    def __init__(self, debug=False):
+        self.debug = debug
+
+    def insert(self, dic, k):
+        if k not in dic or dic[k] == 0:
+            dic[k] = 1
+            return False
+        elif dic[k] > 0:
+            dic[k] += 1
+            return True
+        else:
+            raise
+
+    def remove(self, dic, k):
+        if k not in dic or dic[k] < 1:
+            raise
+        dic[k] -= 1
+        return dic[k]
+
+    def solution(self, s):
+        s_dict = dict()
+        answer, answer_s = 0, ''
+        front, back = 0, 0
+        while back < len(s):
+            is_dup = self.insert(s_dict, s[back])
+            back += 1
+            if self.debug:
+                print(front, back, s_dict, is_dup)
+            if is_dup:
+                while True:
+                    remain = self.remove(s_dict, s[front])
+                    front += 1
+                    if self.debug:
+                        print('  ', front, back, s_dict)
+                    if s[back - 1] == s[front - 1] and remain <= 1 or front >= back:
+                        break
+            if back - front > answer:
+                answer = back - front
+                answer_s = s[front:back]
+                if self.debug:
+                    print(answer_s, answer)
+        return answer
+
+    def comparison_solution(self, s):
+        answer = ''
+        for i in range(len(s)):
+            for j in range(i + 1, len(s) + 1):
+                if len(set(list(s[i:j]))) < j - i:
+                    continue
+                elif len(answer) < j - i:
+                    answer = s[i:j]
+        return len(answer)
+
+
+    def test_one_random(self):
+        n = random.randint(0, 100)
+        s = ''.join([random.choice(string.ascii_lowercase) for _ in range(n)])
+        answer1 = self.solution(s)
+        answer2 = self.comparison_solution(s)
+
+        print(answer1, answer2)
+        assert answer1 == answer2
