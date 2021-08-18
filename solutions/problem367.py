3145560b0b85c195e90f7720e289d4d2918f7fb6
kjunm000n <88933686+kjunm000n@users.noreply.github.com>
2021-08-19 00:12:49 +0900

    Prob 367 (#7)
    * Add) Problem1 solution
    
    * Solve) prob-367
    
    Co-authored-by: kjunm000n <kjunm000n@github.com>

---
+++ solutions/problem367.py
@@ -0,0 +1,41 @@
+# [367] Valid Perfect Square
+
+import random
+
+from solutions.interface import ProblemInterface
+
+
+class Problem367(ProblemInterface):
+    def __init__(self, debug=False):
+        self.debug = debug
+
+    def solution(self, num: int) -> bool:
+        max_sqrt = 2 ** 16
+        curr = max_sqrt // 2 - 1
+        step_size = max_sqrt // 4
+        while True:
+            if num < curr * curr:
+                curr -= step_size
+            elif num > (curr + 1) * (curr + 1):
+                curr += step_size
+            elif curr * curr == num or (curr + 1) * (curr + 1) == num:
+                return True
+            if step_size == 0:
+                return False
+            step_size //= 2
+
+    def comparison_solution(self, x: int) -> bool:
+        from math import sqrt
+        return int(sqrt(x)) ** 2 == x
+
+    def test_one_random(self, num_size=2 ** 31):
+        num = random.randint(0, num_size)
+        answer1 = self.solution(num)
+        answer2 = self.comparison_solution(num)
+
+        print(answer1, answer2)
+        assert answer1 == answer2
+
+    def test_runner(self, iters=1000):
+        for i in range(iters):
+            self.test_one_random()
