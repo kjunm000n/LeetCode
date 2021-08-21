8b6aadbf88bee5c3f164af013f05650e564566ad
kjunm000n <kjunm000n@github.com>
2021-08-21 21:32:51 +0900

    Solve) prob-6

---
+++ solutions/problem6.py
@@ -0,0 +1,49 @@
+# [6] ZigZag Conversion
+
+import os
+import random
+import string
+
+from solutions.interface import ProblemInterface
+from main import debug_mode
+
+
+class Problem6(ProblemInterface):
+    @ProblemInterface.time_check(debug_mode)
+    def solution(self, s: str, numRows: int) -> str:
+        answer = ''
+        if numRows == 1:
+            return s
+        for i in range(numRows):
+            if i in [0, numRows-1]:
+                line = s[i::2*(numRows-1)]
+            else:
+                line1, line2 = s[i::2*(numRows-1)], s[2*(numRows-1)-i::2*(numRows-1)]
+                line = [None] * (len(line1)+len(line2))
+                line[0::2], line[1::2] = line1, line2
+                line = ''.join(line)
+            answer += line
+        return answer
+
+    @ProblemInterface.time_check(debug_mode)
+    def comparison_solution(self, s: str, numRows: int) -> str:
+        line_dict = {i:[] for i in range(numRows)}
+        for i, c in enumerate(s):
+            line = min(i%((numRows-1)*2), ((numRows-1)*2)-i%((numRows-1)*2))
+            line_dict[line].append(c)
+        answer = ''
+        for i in range(numRows):
+            answer += ''.join(line_dict[i])
+        return answer
+
+    def test_one_random(self, str_len=1000):
+        n = random.randint(1,str_len)
+        numRows = random.randint(1, n)
+        s = ''.join([random.choice(string.ascii_lowercase) for _ in range(str_len)])
+
+        answer1 = self.solution(s, numRows)
+        answer2 = self.comparison_solution(s, numRows)
+
+        if os.getenv('debug_mode'):
+            print(answer1, answer2)
+        assert answer1 == answer2
