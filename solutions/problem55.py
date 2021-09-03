d7788cf2349c7a3adf8d6a1a7d3d40cfb8a0a030
kjunm000n <88933686+kjunm000n@users.noreply.github.com>
2021-09-03 23:07:52 +0900

    Solve) prob-55 (#33)
    Co-authored-by: kjunm000n <kjunm000n@github.com>

--- solutions/problem55.py
+++ solutions/problem55.py
@@ -1,4 +1,4 @@
-# [45] Jump Game II
+# [55] Jump Game
 
 import random
 from typing import Optional, Union, Any, List, Dict, Set
@@ -7,45 +7,46 @@ from main import debug_mode
 from solutions.interface import ProblemInterface, Difficulty
 
 
-class Problem45(ProblemInterface):
+class Problem55(ProblemInterface):
     difficulty = Difficulty.Medium
 
     @ProblemInterface.time_check(debug_mode)
-    def solution(self, nums: List[int]) -> int:
+    def solution(self, nums: List[int]) -> bool:
         u""" time complexity: O(n) """
-        min_jump, curr_pos = 0, 0
+        curr_pos = 0
         while True:
-            if curr_pos == len(nums)-1:
-                break
-            if nums[curr_pos] + curr_pos >= len(nums)-1:
-                min_jump += 1
+            if curr_pos == len(nums) - 1 or nums[curr_pos] + curr_pos >= len(nums) - 1:
                 break
             next_pos = curr_pos
             for i in range(1, min(nums[curr_pos] + 1, len(nums) - curr_pos)):
                 if nums[next_pos] + next_pos < nums[curr_pos + i] + curr_pos + i:
                     next_pos = curr_pos + i
+            if next_pos == curr_pos:
+                return False
             curr_pos = next_pos
-            min_jump += 1
-        return min_jump
+        return True
 
     @ProblemInterface.time_check(debug_mode)
-    def comparison_solution(self, nums: List[int]) -> int:
+    def comparison_solution(self, nums: List[int]) -> bool:
         u""" time complexity: O(n^2) """
         min_jump = [len(nums) for _ in nums]
         min_jump[0] = 0
+        visited = set()
         pos_queue = [0]
-        visited = {0}
-        while pos_queue:
+        for i in range(len(nums)):
+            if not pos_queue:
+                return False
             curr = pos_queue.pop(0)
-            for i in range(max(curr-nums[curr], 0), min(curr+nums[curr]+1, len(nums))):
-                if i not in visited:
-                    visited.add(i)
-                    min_jump[i] = min_jump[curr] + 1
-                    pos_queue.append(i)
-                if i == len(nums)-1:
-                    return min_jump[i]
+            for j in range(max(0, curr - nums[curr]), min(len(nums), curr + nums[curr] + 1)):
+                if j not in visited:
+                    visited.add(j)
+                    pos_queue.append(j)
+                    min_jump[j] = min_jump[curr]
+                    if j == len(nums) - 1:
+                        return True
+        return False
 
-    def test_one_random(self, num_length_size=10**4, num_size=10**3):
+    def test_one_random(self, num_length_size=10**4, num_size=10**5):
         num_length = random.randint(1, num_length_size)
         nums = [random.randint(0, num_size) for _ in range(num_length)]
         answer1 = self.solution(nums)
